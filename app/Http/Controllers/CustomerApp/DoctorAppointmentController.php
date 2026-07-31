df4736a49a15602dac5d48006c234acd639da378
Yaswanth <yaswanth@Rajus-MacBook-Pro.local>
2026-07-31 12:29:39 +0530

    reschedule appointment

--- app/Http/Controllers/CustomerApp/DoctorAppointmentController.php
+++ app/Http/Controllers/CustomerApp/DoctorAppointmentController.php
@@ -219,4 +219,220 @@ class DoctorAppointmentController extends Controller
             'message' => 'Appointments Fetched Successfully'
         ]);
     }
+
+    public function cancelAppointment(Request $request)
+    {
+        $customer = auth('sanctum')->user();
+
+        if (!$customer) {
+            return response()->json([
+                'success' => 0,
+                'message' => 'Please Login'
+            ], 401);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'appointment_id' => 'required|exists:doctor_appointments,id',
+            'cancel_reason' => 'nullable|string|max:500',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => 0,
+                'message' => $validator->errors()->first()
+            ]);
+        }
+
+        $appointment = DoctorAppointment::where('id', $request->appointment_id)
+            ->where('customer_id', $customer->id)
+            ->first();
+
+        if (!$appointment) {
+            return response()->json([
+                'success' => 0,
+                'message' => 'Appointment not found.'
+            ]);
+        }
+
+        if ($appointment->appointment_status == 'cancelled') {
+            return response()->json([
+                'success' => 0,
+                'message' => 'Appointment already cancelled.'
+            ]);
+        }
+
+        if (Carbon::parse($appointment->appointment_date . ' ' . $appointment->appointment_time)->isPast()) {
+            return response()->json([
+                'success' => 0,
+                'message' => 'Past appointments cannot be cancelled.'
+            ]);
+        }
+
+        $appointment->appointment_status = 'cancelled';
+        $appointment->cancel_reason = $request->cancel_reason;
+        $appointment->cancelled_at = now();
+        $appointment->save();
+
+        return response()->json([
+            'success' => 1,
+            'message' => 'Appointment cancelled successfully.'
+        ]);
+    }
+
+    public function rescheduleAppointment(Request $request)
+    {
+        $customer = auth('sanctum')->user();
+
+        if (!$customer) {
+            return response()->json([
+                'success' => 0,
+                'message' => 'Please Login'
+            ], 401);
+        }
+
+        $validator = Validator::make($request->all(), [
+
+            'appointment_id' => 'required|exists:doctor_appointments,id',
+
+            'doctor_schedule_id' => 'required|exists:doctor_schedules,id',
+
+            'appointment_date' => 'required|date',
+
+            'appointment_time' => 'required',
+
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => 0,
+                'message' => $validator->errors()->first()
+            ]);
+        }
+
+        $appointment = DoctorAppointment::where('id', $request->appointment_id)
+            ->where('customer_id', $customer->id)
+            ->first();
+
+        if (!$appointment) {
+            return response()->json([
+                'success' => 0,
+                'message' => 'Appointment not found.'
+            ]);
+        }
+        if (
+            $appointment->doctor_schedule_id == $request->doctor_schedule_id &&
+            $appointment->appointment_date == $request->appointment_date &&
+            Carbon::parse($appointment->appointment_time)->format('H:i') ==
+            Carbon::parse($request->appointment_time)->format('H:i')
+        ) {
+            return response()->json([
+                'success' => 0,
+                'message' => 'This appointment is already scheduled for the selected date and time.'
+            ]);
+        }
+        $oldAppointmentDateTime = Carbon::parse(
+            $appointment->appointment_date . ' ' . $appointment->appointment_time
+        );
+
+        if ($oldAppointmentDateTime->isPast()) {
+            return response()->json([
+                'success' => 0,
+                'message' => 'Past appointments cannot be rescheduled.'
+            ]);
+        }
+
+        if ($appointment->appointment_status == 'cancelled') {
+            return response()->json([
+                'success' => 0,
+                'message' => 'Cancelled appointment cannot be rescheduled.'
+            ]);
+        }
+
+        $appointmentDateTime = Carbon::parse(
+            $request->appointment_date . ' ' . $request->appointment_time
+        );
+
+        if ($appointmentDateTime->lte(now())) {
+            return response()->json([
+                'success' => 0,
+                'message' => 'Please select a future date and time.'
+            ]);
+        }
+
+        $schedule = DoctorSchedule::where('id', $request->doctor_schedule_id)
+            ->where('doctor_id', $appointment->doctor_id)
+            ->where('status', 1)
+            ->first();
+
+        if (!$schedule) {
+            return response()->json([
+                'success' => 0,
+                'message' => 'Doctor schedule not found.'
+            ]);
+        }
+
+        $requestedTime = Carbon::parse($request->appointment_time)->format('H:i');
+
+        $start = Carbon::parse($schedule->available_from);
+        $end = Carbon::parse($schedule->available_to);
+
+        $isValidSlot = false;
+
+        while ($start < $end) {
+
+            if (
+                $start->format('H:i') >= '13:00' &&
+                $start->format('H:i') < '14:00'
+            ) {
+                $start->addMinutes($schedule->slot_duration);
+                continue;
+            }
+
+            if ($start->format('H:i') == $requestedTime) {
+                $isValidSlot = true;
+                break;
+            }
+
+            $start->addMinutes($schedule->slot_duration);
+        }
+
+        if (!$isValidSlot) {
+            return response()->json([
+                'success' => 0,
+                'message' => 'Invalid appointment slot.'
+            ]);
+        }
+
+        $alreadyBooked = DoctorAppointment::where('doctor_id', $appointment->doctor_id)
+            ->whereDate('appointment_date', $request->appointment_date)
+            ->whereTime('appointment_time', $request->appointment_time)
+            ->where('id', '!=', $appointment->id)
+            ->whereNotIn('appointment_status', ['cancelled'])
+            ->exists();
+
+        if ($alreadyBooked) {
+            return response()->json([
+                'success' => 0,
+                'message' => 'Selected slot already booked.'
+            ]);
+        }
+
+        $appointment->doctor_schedule_id = $request->doctor_schedule_id;
+        $appointment->appointment_date = $request->appointment_date;
+        $appointment->appointment_time = $request->appointment_time;
+        $appointment->appointment_status = 'pending';
+
+        $appointment->token_no = DoctorAppointment::where('doctor_id', $appointment->doctor_id)
+            ->whereDate('appointment_date', $request->appointment_date)
+            ->where('id', '!=', $appointment->id)
+            ->count() + 1;
+
+        $appointment->save();
+
+        return response()->json([
+            'success' => 1,
+            'message' => 'Appointment rescheduled successfully.',
+            'data' => $appointment
+        ]);
+    }
 }
--- routes/api/customer.php
+++ routes/api/customer.php
@@ -31,17 +31,22 @@ Route::post('verifyMobile', 'AccountController@verifyMobile');
 Route::post('resendOtp', 'AccountController@resendOtp');
 
 Route::group(['middleware' => ['customertokenCheck']], function () {
+    //Profile
     Route::get('logout', 'AccountController@logout');
     Route::get('profile', 'ProfileController@profile');
     Route::post('updateProfile', 'ProfileController@updateProfile');
     Route::post('/family-member/store', [ProfileController::class, 'storeFamilyMember']);
     Route::post('/family-member/update', [ProfileController::class, 'updateFamilyMember']);
     Route::post('/delete-family-member', [ProfileController::class, 'deleteFamilyMember']);
+
     //hospitals
     Route::any('hospitals', "HospitalController@hospitals");
     Route::any('specializations', "HospitalController@specializations");
+
+    //Doctors
     Route::any('doctors', "DoctorController@doctors");
     Route::any('doctor-slots', [DoctorController::class, 'availableSlots']);
+
     //diagnostics
     Route::any('diagnostics', [DiagnosticController::class, 'diagnostics']);
     Route::any('lab-tests', [DiagnosticController::class, 'labTests']);
@@ -56,8 +61,11 @@ Route::group(['middleware' => ['customertokenCheck']], function () {
     Route::any('ambulances', [AmbulanceController::class, 'ambulances']);
     Route::get('ambulances/{id}', [AmbulanceController::class, 'ambulanceDetails']);
 
+    //Doctor Appointments
     Route::post('book-doctor-appointment', [DoctorAppointmentController::class, 'bookDoctorAppointment']);
     Route::any('myAppointments', [DoctorAppointmentController::class, 'myAppointments']);
+    Route::post('cancel-appointment', [DoctorAppointmentController::class, 'cancelAppointment']);
+    Route::post('reschedule-appointment', [DoctorAppointmentController::class, 'rescheduleAppointment']);
 });
 Route::any('/states', [LocationController::class, 'states']);
 Route::any('/postalDetails', [LocationController::class, 'postalDetails']);
